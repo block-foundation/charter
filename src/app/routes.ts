1eca78639cf27baac3ba4fbf877581747500a11b
Lars van Vianen <lars@vianen.com>
2023-07-27 16:56:17 +0200

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,7 @@ import { Route } from "./core/interfaces/routeconf.interface";
 import { ContactPage } from "./pages/contact/contact.page";
 import { HomePage } from "./pages/home/home.page";
 
+
 export const AppRoutes:Route[] = [
     {
         path: "/",
