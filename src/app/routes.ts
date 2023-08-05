98a6df950ec22c3867302047b5fd987e3b9a160b
Lars van Vianen <lars@vianen.com>
2023-08-05 12:12:26 +0200

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,13 +3,13 @@ import { ContactPage } from "./pages/contact/contact.page";
 import { HomePage } from "./pages/home/home.page";
 
 
-export const AppRoutes:Route[] = [
-    {
-        path: "/",
-        pageComponent: HomePage
-    },
-    {
-        path: "/contact",
-        pageComponent: ContactPage
-    }
+export const AppRoutes: Route[] = [
+    // {
+    //     path: "/",
+    //     pageComponent: HomePage
+    // },
+    // {
+    //     path: "/contact",
+    //     pageComponent: ContactPage
+    // }
 ]
